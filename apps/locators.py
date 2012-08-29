d19b17e4df9a65981750a5998d7bb02a51419157
James Laska <jlaska@redhat.com>
2012-08-29 16:22:00 -0400

    Comment cleanup

---
+++ apps/locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+"""
+Common locators for all products are contained within.
+"""
+
+class BaseLocators(object):
+    '''Base class for common locators'''
+
+    # some_locator_by_id = (By.ID, 'someLocator')
+    # some_locator_by_css = (By.CSS_SELECTOR, '#someLocator')
+    # some_locator_by_xpath = (By.XPATH, "//div[@id='someLocator']")
+    # some_elements_locator = (By.CSS_SELECTOR, 'li .someElementsLocator')
