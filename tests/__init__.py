bb48574e6f996d7c216c3c1eb5ca37ef0e3a28d8
James Laska <jlaska@redhat.com>
2012-09-05 15:25:13 -0400

    Add support for setup and teardown @classmethods
     * Also add load_page() convenience method (shorter than typing
       self.katello.load_page...)
     * Store mozwebqa configuration in self.testsetup
     * Create handle to access katello ApiTasks (self.api)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 import pytest
 import apps
+import random
+import string
 
 class Base_Test(object):
     '''
@@ -10,6 +12,33 @@ class Base_Test(object):
         """ setup any state specific to the execution of the given class (which
         usually contains tests).
         """
-        test_setup = pytest.config.pluginmanager.getplugin("mozwebqa")
-        self.app = apps.initializeProduct(test_setup.TestSetup)
+        self.testsetup = pytest.config.pluginmanager.getplugin("mozwebqa").TestSetup
+        self.app = apps.initializeProduct(self.testsetup)
 
+    @classmethod
+    def teardown_class(self):
+        '''
+        Perform any required test teardown
+        '''
+
+    def load_page(self, page_name):
+        '''
+        Description:
+            Convenience method to return an initialized application page
+        Returns:
+            object of type BasePage
+        '''
+        return self.app.load_page(page_name)
+
+    def random_str(self, prefix="", str_len=5, suffix=""):
+        '''
+        Description:
+            Return a string containing a random sample of ascii letters and digits
+        Parameters:
+            prefix (string): prefix random string with supplied string
+            str_len (digit): length of string to generate
+            suffix (string): append random string with supplied suffix
+        Returns:
+            string
+        '''
+        return prefix + ''.join(random.sample(string.ascii_letters + string.digits, str_len)) + suffix
--- tests/katello2/__init__.py
+++ tests/katello2/__init__.py
@@ -1,11 +1,24 @@
 import pytest
 import apps
+from api.api import ApiTasks
 from tests import Base_Test
 
 class Katello_Test(Base_Test):
     '''
     Base katello test class
     '''
+
+    @classmethod
+    def setup_class(self):
+        """ setup any state specific to the execution of the given class (which
+        usually contains tests).
+        """
+        Base_Test.setup_class.im_func(self)
+        self.api = ApiTasks(self.testsetup)
+
+    @classmethod
+    def teardown_class(self):
+        Base_Test.teardown_class.im_func(self)
 
     @property
     def katello(self):
