5c8d1b7eeb9f345fb1271d32251bd431a1bad915
James Laska <jlaska@redhat.com>
2012-09-06 15:46:00 -0400

    Be sure to call parent setup/teardown methods

--- tests/aeolus2/__init__.py
+++ tests/aeolus2/__init__.py
@@ -10,3 +10,14 @@ class Aeolus_Test(Base_Test):
     @property
     def aeolus(self):
         return self.app
+
+    @classmethod
+    def setup_class(self):
+        Base_Test.setup_class.im_func(self)
+        # FIXME - assign self.api when API is available
+        # self.api = ApiTasks(self.testsetup)
+
+    @classmethod
+    def teardown_class(self):
+        Base_Test.teardown_class.im_func(self)
+
