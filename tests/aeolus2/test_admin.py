a73a3cad8e15aea54e9ddff0f4521814f5ab96ee
aweiteka <aweiteka@redhat.com>
2012-09-12 13:35:48 -0400

    fixed skipif, commented out login assert, import data/assert_response.py, test_add_users_to_user_groups()

--- tests/aeolus2/test_admin.py
+++ tests/aeolus2/test_admin.py
@@ -3,7 +3,7 @@
 import pytest
 import apps
 from data.large_dataset import Admin
-from data.assert_messages import *
+from data.assert_response import *
 from tests.aeolus2 import Aeolus_Test
 
 def setup_module(module):
@@ -38,8 +38,8 @@ class TestAdmin(Aeolus_Test):
         Create users
         '''
         page = self.aeolus.load_page('Aeolus')
-        #page.login()
-        assert page.login() == aeolus_msg['login']
+        page.login()
+        #assert page.login() == aeolus_msg['login']
 
         for user in Admin.users:
             assert page.create_user(user) == aeolus_msg['add_user']
@@ -57,37 +57,33 @@ class TestAdmin(Aeolus_Test):
         #else:
         #    assert page.logout() == "Aeolus Conductor | Login"
 
-    @pytest.mark.skipif("Aeolus.product_ver == '1.0.1'")
+    @pytest.mark.skipif("config.getvalue('product_version') == '1.0.1'")
     @pytest.mark.user_admin
     @pytest.mark.aeolus_setup
     def test_create_user_groups(self, mozwebqa):
         '''
         create user groups
         '''
-        # FIXME: conver to new format
-        home_page = Home(mozwebqa)
-        #assert home_page.login() == "Login successful!"
-        home_page.login()
-
-        page = Aeolus(mozwebqa)
+        page = self.aeolus.load_page('Aeolus')
+        page.login()
+        #assert page.login() == aeolus_msg['login']
 
         for user_group in Admin.user_groups:
             assert page.create_user_group(user_group) == "User Group added"
 
         # test cleanup
-        if page.test_cleanup == True:
-            for user_group in Admin.user_groups:
-                assert page.delete_user_group(user_group["name"]) == \
-                       "Deleted user group " + user_group["name"]
+        #if page.test_cleanup == True:
+        #    for user_group in Admin.user_groups:
+        #        assert page.delete_user_group(user_group["name"]) == \
+        #               "Deleted user group " + user_group["name"]
 
-        assert page.logout() == "Aeolus Conductor | Login"
+        #assert page.logout() == "Aeolus Conductor | Login"
 
-    @pytest.mark.skipif("Aeolus.product_ver == '1.0.1'")
+    @pytest.mark.skipif("config.getvalue('product_version') == '1.0.1'")
     def test_add_users_to_user_groups(self, mozwebqa):
-        home_page = Home(mozwebqa)
-        assert home_page.login() == "Login successful!"
-
-        page = Aeolus(mozwebqa)
+        page = self.aeolus.load_page('Aeolus')
+        page.login()
+        #assert page.login() == aeolus_msg['login']
 
         # move to aeolus_page.py
         # capture user IDs from "/users/" view
@@ -97,4 +93,22 @@ class TestAdmin(Aeolus_Test):
         # capture user_group IDs from "/user_groups/" view
         for user_group in Admin.user_groups:
             user_group["id"] = page.get_user_group_id(user_group["name"])
+
+        # add users to groups
+        for group in Admin.user_groups:
+            for user in Admin.users:
+                if group['name'] in user['user_groups']:
+                    assert page.add_user_to_group(group['id'], user['id']) ==\
+                        aeolus_msg['add_user_to_group'] % \
+                            user['fname'] + ' ' + user['lname']
+
+         # test cleanup
+         #if self.testsetup.test_cleanup:
+         #    for group in Admin.user_groups:
+         #        for user in Admin.users:
+         #            if group['name'] in user['user_groups']:
+         #                assert page.delete_user_from_group(group['id'], \
+         #                    user['id']) == \
+         #                    aeolus_msg['delete_user_from_group'] % \
+         #                    user['fname'] + ' ' + user['lname']
 
