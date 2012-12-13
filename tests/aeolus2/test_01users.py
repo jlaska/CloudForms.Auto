449b43e294110b6ba3408732d9fa6533f8159fd6
James Laska <jlaska@redhat.com>
2012-12-13 11:58:06 -0500

    Test cleanup

--- tests/aeolus2/test_01users.py
+++ tests/aeolus2/test_01users.py
@@ -21,24 +21,10 @@ class TestUsers(Aeolus_Test):
         Create users
         '''
         page = self.aeolus.load_page('Aeolus')
-        page.login(user='admin', password='password')
-        #assert page.login() == aeolus_msg['login']
+        page.login()
 
         for user in Admin.users:
             assert page.create_user(user) == aeolus_msg['add_user']
-
-        # TODO: not needed if teardown_class working
-        # test cleanup
-        #if pytest.config.getvalue('test.cleanup') == True:
-        #    for user in Admin.users:
-        #        assert page.delete_user(user["username"]) == \
-        #               "User has been successfully deleted."
-
-        #if page.product_ver == '1.0.1':
-        #    page.logout()
-        #else:
-        #    assert page.logout() == "Aeolus Conductor | Login"
-
 
     @pytest.mark.skipif("config.getvalue('project-version') == '1.0.1'")
     def test_create_user_groups(self):
@@ -46,25 +32,15 @@ class TestUsers(Aeolus_Test):
         create user groups
         '''
         page = self.aeolus.load_page('Aeolus')
-        page.login(user='admin', password='password')
-        #assert page.login() == aeolus_msg['login']
+        page.login()
 
         for user_group in Admin.user_groups:
             assert page.create_user_group(user_group) == "User Group added"
 
-        # test cleanup
-        #if pytest.config.getvalue('test-cleanup') == True:
-        #    for user_group in Admin.user_groups:
-        #        assert page.delete_user_group(user_group["name"]) == \
-        #               "Deleted user group " + user_group["name"]
-
-        #assert page.logout() == "Aeolus Conductor | Login"
-
     @pytest.mark.skipif("config.getvalue('project-version') == '1.0.1'")
     def test_add_users_to_user_groups(self):
         page = self.aeolus.load_page('Aeolus')
-        page.login(user='admin', password='password')
-        #assert page.login() == aeolus_msg['login']
+        page.login()
 
         # capture user IDs from "/users/" view
         for user in Admin.users:
@@ -82,20 +58,10 @@ class TestUsers(Aeolus_Test):
                         aeolus_msg['add_user_to_group'] % \
                             user['fname'] + ' ' + user['lname']
 
-         # test cleanup
-         #if pytest.config.getvalue('test-cleanup')
-         #    for group in Admin.user_groups:
-         #        for user in Admin.users:
-         #            if group['name'] in user['user_groups']:
-         #                assert page.delete_user_from_group(group['id'], \
-         #                    user['id']) == \
-         #                    aeolus_msg['delete_user_from_group'] % \
-         #                    user['fname'] + ' ' + user['lname']
-
     @pytest.mark.setup
     def test_add_permissions(self):
         page = self.aeolus.load_page('Aeolus')
-        page.login(user='admin', password='password')
+        page.login()
 
         for user_group in Admin.user_groups:
             page.grant_permissions("group", user_group)
